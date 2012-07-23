5d689ba9808494cb767bf53415cc2bd1d94faed0
Michael Forrest <michael.forrest@gmail.com>
2012-07-23 12:21:37 +0100

    removed library calendar

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -8,42 +8,10 @@ class AppDelegate
     @settings_button = UIBarButtonItem.alloc.initWithTitle "⚙",style: UIBarButtonItemStyleBordered, target:self, action:'show_settings'
     @calendar.navigationItem.rightBarButtonItem = @settings_button
 
-    @kal = KalViewController.alloc.init
-    @window.rootViewController.pushViewController @kal, animated: true
-    
-    @marked_dates = [NSDate.date]
-    @kal.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithTitle "Today", style: UIBarButtonItemStyleBordered, target: self, action: 'showAndSelectToday'
-    @kal.dataSource = self
-
     @window.makeKeyAndVisible
     true
   end
   
-  def presentingDatesFrom fromDate, to: toDate, delegate: delegate
-    NSLog "from: #{fromDate}"
-    @calendar_callback = delegate
-    delegate.loadedDataSource self
-  end
-  def markedDatesFrom fromDate, to: toDate
-    NSLog "Marked dates from #{fromDate} to #{toDate}"
-    @marked_dates
-  end
-  def loadItemsFromDate fromDate, toDate: toDate # when a cell is selected
-    NSLog "Load items from date #{fromDate} to #{toDate}" # 
-    @marked_dates << fromDate unless @marked_dates.include? fromDate
-    # @calendar_callback.loadedDataSource( self ) if @calendar_callback
-  end
-  def removeAllItems
-  end
-  def tableView tableView, cellForRowAtIndexPath: indexPath
-  end
-  def tableView tableView, numberOfRowsInSection: section
-    0
-  end
-  
-  def showAndSelectToday
-    @kal.showAndSelectDate NSDate.date
-  end
   
   def show_settings
     settings = SettingsScreen.alloc.init
